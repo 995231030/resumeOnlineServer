d28dee7d15d79f5c1756ec1b697f0d2f33815fa2
朱永博 <995231030@qq.com>
2022-10-21 17:59:51 +0800

    normal

--- app/service/mainService.ts
+++ app/service/mainService.ts
@@ -13,7 +13,7 @@ export default class mainService extends Service {
             getVerificationCode: getVerificationCode()
         }
     }
-    // 登录
+    // 登录  
     public async userLogin(req: object | any) {
         let res: any = await this.service.mysqlApi.query("userList", "userEmail", req.data.account)
         // let res: any = await this.app.mysql.query(`select * from userlist where userEmail = '${req.data.account}'`)
